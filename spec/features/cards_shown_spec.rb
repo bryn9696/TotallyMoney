3270e82b8047370a7c41befff37d88088dbcf86f
bryn9696 <bryn2009@live.co.uk>
2022-03-18 14:48:18 +0000

    written tests for what final web app should be able to output

--- spec/features/cards_shown_spec.rb
+++ spec/features/cards_shown_spec.rb
@@ -3,4 +3,32 @@ feature 'A user can see card choices' do
     user_details_student
     expect(page).to have_content 'Anywhere Card:'
   end
+
+  scenario 'A user sees anywhere card when doesnt match other conditions' do
+    user_details_low_fulltime 
+    expect(page).to have_content 'Anywhere Card:'
+    expect(page).to have_content '£300'
+  end
+
+  scenario 'A user sees all three applicable cards' do
+    user_details_paid_student
+    expect(page).to have_content 'Anywhere Card:'
+    expect(page).to have_content 'Student Life:'
+    expect(page).to have_content 'Liquid Card:'
+    expect(page).to have_content '£4500'
+  end
+
+  scenario 'A user sees all the two applicable cards' do
+    user_details_high_fulltime
+    expect(page).to have_content 'Anywhere Card:'
+    expect(page).to have_content 'Liquid Card:'
+    expect(page).to have_content '£3300'
+  end
+
+  scenario 'A user sees all the two applicable cards' do
+    user_details_student
+    expect(page).to have_content 'Anywhere Card:'
+    expect(page).to have_content 'Student Life:'
+    expect(page).to have_content '£1500'
+  end
 end
--- spec/features/user_details_spec.rb
+++ spec/features/user_details_spec.rb
@@ -1,6 +1,6 @@
 feature 'A user can enter details' do
   scenario 'A user enters details form' do
     user_details_student
-    expect(page).to have_content 'Bryn'
+    expect(page).to have_content 'Hello, Bryn!'
   end
 end
